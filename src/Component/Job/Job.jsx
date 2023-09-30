a55478dcaec81c1737822b665550c7de2d21b32c
Arjun <arjundasback515@gmail.com>
2023-10-01 00:18:51 +0600

    hader section design done

--- src/Component/Job/Job.jsx
+++ src/Component/Job/Job.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import JobCart from './JobCart';
 
 const Job = () => {
+    const jobDAta = [
+        {
+            id:1,
+            name:"arjun"
+        },
+        {
+            id:2,
+            name:"arjun"
+        }
+    ]
     return (
-        <div>
-            this is job
+        <div className='mt-10'>
+            <div>
+                <ul className='flex justify-between my-3 font-Inter text-xl'>
+                    <li>1.5k Jobs found</li>
+                    <li className=' text-[#999999]'>1.5k Jobs found</li>
+                    <li className=' text-[#999999]'>1.5k Jobs found</li>
+                    <li className=' text-[#999999]'>1.5k Jobs found</li>
+                    <li className=' text-[#999999]'>1.5k Jobs found</li>
+                </ul>
+                <hr />
+            </div>
+            {
+                jobDAta?.map(data=> <JobCart data={data} key={data.id}></JobCart>)
+            }
         </div>
     );
 };
--- src/Component/Job/JobCart.jsx
+++ src/Component/Job/JobCart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const JobCart = () => {
     return (
         <div>
-            
+            this is job
         </div>
     );
 };
