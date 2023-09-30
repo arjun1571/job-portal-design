a91faec18e2db4902590efc8f14c96e6b2599fa0
Arjun <arjundasback515@gmail.com>
2023-10-01 00:55:08 +0600

    build: create the job page card

--- src/Component/Job/Job.jsx
+++ src/Component/Job/Job.jsx
@@ -15,18 +15,20 @@ const Job = () => {
     return (
         <div className='mt-10'>
             <div>
-                <ul className='flex justify-between my-3 font-Inter text-xl'>
+                <ul className='flex   my-3 font-Inter md:text-xl text-xs p-3'>
                     <li>1.5k Jobs found</li>
-                    <li className=' text-[#999999]'>1.5k Jobs found</li>
-                    <li className=' text-[#999999]'>1.5k Jobs found</li>
-                    <li className=' text-[#999999]'>1.5k Jobs found</li>
-                    <li className=' text-[#999999]'>1.5k Jobs found</li>
+                    <li className='md:mx-8 mx-2 text-[#999999]'>Requested (4)</li>
+                    <li className='md:mx-8 mx-2 text-[#999999]'>Shortlisted (2)</li>
+                    <li className='md:mx-8 mx-2 text-[#999999]'>Declined(2) </li>
+                    <li className='md:mx-8 mx-2 text-[#999999]'>Hired(4) </li>
                 </ul>
                 <hr />
             </div>
+            <div className='my-3'>
             {
                 jobDAta?.map(data=> <JobCart data={data} key={data.id}></JobCart>)
             }
+            </div>
         </div>
     );
 };
--- src/Component/Job/JobCart.jsx
+++ src/Component/Job/JobCart.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import prifile from "../../assets/Ellipse 618.png";
 
 const JobCart = () => {
-    return (
-        <div>
-            this is job
+  return (
+    <div className="md:flex justify-between border my-3 md:p-10 p-4 bg-[#FFFFFF]">
+      <div>
+        <div className="flex items-center">
+          <img src={prifile} alt="" className="h-20 w-20" />
+          <div className="md:mx-4 mx-2">
+            <h1 className="text-2xl font-bold text-[#242424]">Headline of the job</h1>
+            <div className="flex justify-between my-2">
+              <p className="">ABGroup</p>
+              <p className="mx-5">ABGroup</p>
+              <p className="">ABGroup</p>
+            </div>
+          </div>
         </div>
-    );
+        <div>
+          <p className="md:mx-5 my-5">
+            AB Group Building is a renowned construction company that
+            specializes in delivering exceptional building solutions. With a
+            strong focus on quality craftsmanship and client... Read More
+          </p>
+        </div>
+      </div>
+      <div>
+        <p>Posted: 1 Week ago</p>
+        <div className="mt-14 flex">
+        <button className="border flex-none px-5 py-2">View</button>
+        <button className="border  flex-none mx-3 px-5 py-2">Apply job</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobCart;
